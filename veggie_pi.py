1319065bd06def5266679e2916cb12febcfa8aa0
Snakespeare1447 <marco.stixenberger@gmail.com>
2019-04-05 12:46:16 +0200

    pin declaration

--- veggie_pi.py
+++ veggie_pi.py
@@ -40,14 +40,22 @@ def DHT_read(sensor, pin):
 
     
 ##########################################################   SETUP  #################################################################################
+#setup is executed once at startup
+#pin declaration:
+#lamp and pump are connected to the double relais module
+#fan1: humidity regulation, fan2: inhouse ventilation (air movement)
 lamp_pin = 17
 pump_pin = 27
+fan1_pin = 22 
 dht1_pin = 4
 lamp = io.LED(pin=lamp_pin, active_high=False)
 pump = io.LED(pin=pump_pin, active_high=False)
 dht1 = Adafruit_DHT.DHT22
 
 
+#variable initialisation:
+temperature = 0
+humidity = 0
 
 
 
