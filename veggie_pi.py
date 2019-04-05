2cbed02f9ae67c25e558fb6274a0af1a5f8cad37
Snakespeare1447 <marco.stixenberger@gmail.com>
2019-04-05 18:34:28 +0200

    buzz

--- veggie_pi.py
+++ veggie_pi.py
@@ -38,7 +38,7 @@ def DHT_read(sensor, pin):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     return (round(humidity, 1), round(temperature, 1))
     
-def beep():
+def beep(buzzer):
     buzzer.play(Tone(500.0)) # Hz
 sleep(0.15)
 buzzer.stop()
