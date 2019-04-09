c80891a3a1cdc826aded29f516added5898a9146
Snakespeare1447 <marco.stixenberger@gmail.com>
2019-04-09 14:41:24 +0200

    imports

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #####################################################################################################################################################
 ########################                                    HEADER                              #####################################################
 #####################################################################################################################################################
+
+
+########################################################### IMPORTS #################################################################################
 from functions import *
-from imports import *
+
+import time
+from time import sleep
+import os
+import matplotlib.pyplot as plt
+#import pandas as pd
+import numpy as np
+import gpiozero as io
+from gpiozero.tones import Tone 
+from smbus2 import SMBusWrapper, i2c_msg               #for i2c devices
+import Adafruit_DHT
+import Adafruit_Python_SSD1306                                #oled
+
+
+
 
 
 
