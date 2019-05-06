597b1684987d8b2c94531432f6747f510f27ae65
Snakespeare1447 <marco.stixenberger@gmail.com>
2019-05-06 21:17:50 +0200

    fix

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ while(True):
         daytime = False
     
     #light control:
-    if(daytime==True and humidity>5and humidity<85 and temperature <=37 and temperature >=12):
+    if(daytime==True and humidity>5 and humidity<85 and temperature <=37 and temperature >=12):
         lamp.on()
     else:
         lamp.off()
