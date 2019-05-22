35801cea7c62869032547d3c3cc6c8819f68fc26
Snakespeare1447 <marco.stixenberger@gmail.com>
2019-05-22 22:16:59 +0200

    testing

--- telegramtest.py
+++ telegramtest.py
@@ -2,7 +2,7 @@ from telepot.loop import MessageLoop
 
 import telepot
 bot = telepot.Bot('387161336:AAGKJeu_VaMk0Cn1PyrT0N0cCh3f9ijYwBM')
-bot.getMe()
+#bot.getMe()
 
 def handle(msg):
     pprint(msg)
