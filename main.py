878c95e18ebb88c67ad74e997a94988b1284bba7
Snakespeare1447 <marco.stixenberger@gmail.com>
2019-05-22 19:43:53 +0200

    .

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ while(True):
         pass
 
     #watering
-    if(((hours==8 or hours==14 or hours==19) and hours!=oldhours and emergencystate==False)):
+    if(((hours==8 or hours==14 or hours==20) and hours!=oldhours and emergencystate==False)):
         watering(pump, pumptime)
 
 
