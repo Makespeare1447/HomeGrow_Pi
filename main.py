9c754bb65fd5b00182ab5b0f4204980dd43b87b2
Marco Stixenberger <marco.stixenberger@gmail.com>
2019-05-28 13:59:27 +0200

    cycles up

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ while(True):
         report_per_telegram(bot, chat_id, temperature, humidity, co2, tvoc, cycles, wateringcycles, lampstate)
         
     #plotting and send plot per telegram
-    if (cycles%5==0 and cycles!=0):
+    if (cycles%200==0 and cycles!=0):
         plot_figure(timestamp_list, bot, chat_id, temperature_list, humidity_list, co2_list, tvoc_list, seconds_since_start_list,
         temp_min, temp_max, humidity_min, humidity_max, co2_min, co2_max, tvoc_min, tvoc_max)
         send_plot_per_telegram(bot, chat_id)
