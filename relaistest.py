26d2d35cf2016bc25546cd8c070c2e4bf143cb1e
Marco Stixenberger <marco.stixenberger@gmail.com>
2019-09-13 10:02:48 +0200

    test realsi

--- relaistest.py
+++ relaistest.py
@@ -5,10 +5,11 @@ relais_1 = io.LED(pin=17, active_high=False)
 relais_2 = io.LED(pin=27, active_high=False)
 
 sleep(2)
+relais_1.on()
 relais_2.on()
 sleep(20)
+relais_1.off()
 relais_2.off()
-
 
 
 print('Success! - you reached the end of the Program!')
