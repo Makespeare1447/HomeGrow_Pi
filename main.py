ea22e0cd3852ee8944560e1706f529ff41f3cdec
Marco Stixenberger <marco.stixenberger@gmail.com>
2019-10-18 08:21:54 +0200

    bugfix

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ while(True):
         timestamp = gettimestamp()
 
         #shutdown in the evening :
-        if((hours==21 and hours!=oldhours)):
+        if(hours==21):
             print('good night! - shutting down...')
             bot.send_message(chat_id, text='good night! - shutting down...')
             os.system("sudo shutdown")
